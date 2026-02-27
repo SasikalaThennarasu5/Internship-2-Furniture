2669ef7c5ddc9ff362e2d7e054bcacfe28a07fff
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-02-27 19:24:31 +0530

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/`,
+  baseURL: "https://internship-2-furniture-1.onrender.com/api/",
 });
 
 API.interceptors.request.use((config) => {
