2103affaae0fdd285838a88b1c54d2ad230d1567
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-03-03 23:47:20 +0530

    add

--- config/settings.py
+++ config/settings.py
@@ -167,8 +167,8 @@ SIMPLE_JWT = {
     
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
