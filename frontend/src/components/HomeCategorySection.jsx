dead1d3555fc36d8f3b252197e8a43c57c99c32d
SasikalaThennarasu5 <sasikalathennarasu5@gmail.com>
2026-03-05 22:28:37 +0530

    add

--- frontend/src/components/HomeCategorySection.jsx
+++ frontend/src/components/HomeCategorySection.jsx
@@ -4,13 +4,18 @@ import { Link } from "react-router-dom";
 
 function HomeCategorySection() {
   const [categories, setCategories] = useState([]);
-  
+  const [activeCategory, setActiveCategory] = useState(null); // NEW
 
   useEffect(() => {
     api.get("categories/")
       .then((res) => setCategories(res.data))
       .catch((err) => console.error(err));
   }, []);
+
+  const handleCategoryClick = (id) => {
+    // Toggle open/close
+    setActiveCategory(activeCategory === id ? null : id);
+  };
 
   return (
     <div className="bg-gray-100 py-16 px-20 relative">
@@ -23,30 +28,33 @@ function HomeCategorySection() {
       {/* Category Tabs */}
       <div className="flex justify-between items-center border-b pb-4">
         {categories.map((category) => (
-  <div key={category.id} className="relative group">
-    
-    <button className="text-gray-700 font-medium hover:text-black transition">
-      {category.name}
-    </button>
+          <div key={category.id} className="relative">
+            
+            <button
+              onClick={() => handleCategoryClick(category.id)}
+              className="text-gray-700 font-medium hover:text-black transition"
+            >
+              {category.name}
+            </button>
 
             {/* Dropdown */}
-            {category.subcategories.length > 0 && (
-              <div className="absolute top-8 left-0 hidden group-hover:block bg-white shadow-xl rounded-lg p-6 w-64 z-50 border">
-                {category.subcategories.map((sub) => (
-          <Link
-            key={sub.id}
-            to={`/shop/${sub.slug}`}
-            className="block py-2 text-gray-600 hover:text-black hover:translate-x-1 transition-all duration-200"
-          >
-            {sub.name}
-          </Link>
+            {category.subcategories.length > 0 &&
+              activeCategory === category.id && (
+                <div className="absolute top-8 left-0 bg-white shadow-xl rounded-lg p-6 w-64 z-50 border">
+                  {category.subcategories.map((sub) => (
+                    <Link
+                      key={sub.id}
+                      to={`/shop/${sub.slug}`}
+                      className="block py-2 text-gray-600 hover:text-black hover:translate-x-1 transition-all duration-200"
+                    >
+                      {sub.name}
+                    </Link>
+                  ))}
+                </div>
+              )}
+          </div>
         ))}
-
       </div>
-    )}
-  </div>
-))}      </div>
-
     </div>
   );
 }
